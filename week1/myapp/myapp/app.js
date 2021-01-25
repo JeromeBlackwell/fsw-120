aeb372336aba08b9e8622b13fa1b95d83d14e7dd
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2021-01-25 02:33:11 -0500

    week1

---
+++ week1/myapp/myapp/app.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+function App()
+    return (
+        <div>
+            <h1>To do List</h1>
+
+            <input type="Checkbox"/>
+            <p>Wash clothes</p>
+            
+            <input type="Checkbox"/>
+            <p>Clean the Kitchen</p>
+
+            <input type="Checkbox"/>
+            <p>Clean the Bathroom</p>
+            
+            <input type="Checkbox"/>
+            <p>Take out Trash</p>
+
+        </div>
+    )
+   
+
+export default App
