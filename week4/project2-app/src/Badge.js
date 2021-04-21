c0e406274d9be5fad81b52894ce42ef7084d49d9
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2021-04-21 01:58:12 -0400

    week4

--- week4/project2-app/src/Badge.js
+++ week4/project2-app/src/Badge.js
@@ -7,6 +7,16 @@ function Badge(props) {
         <div>
             <h1>Fsw-120 Week4 Project2</h1>
             {props.firstName}
+            <br></br>
+            {props.lastName}
+            <br></br>
+            {props.email}
+            <br></br>
+            {props.placeOfBirth}
+            <br></br>
+            {props.phone}
+            <br></br>
+            {props.favoriteFood}
         </div>
     )
 }
